030808823210c8306f2542fcc121e092ee00d3ca
Scala Steward <me@scala-steward.org>
2021-06-18 21:45:58 +0200

    Update trace4cats-base, trace4cats-core, ... to 0.12.0-RC1+162-70070fb2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val scala212 = "2.12.14"
     val scala213 = "2.13.6"
 
-    val trace4cats = "0.12.0-RC1+156-7ea07b63"
+    val trace4cats = "0.12.0-RC1+162-70070fb2"
 
     val catsEffect = "3.1.1"
     val http4s = "0.23.0-RC1"
