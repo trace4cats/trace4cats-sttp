f1b7b6ea44a227f3a8fc240c7842abe19a9d3f6b
Scala Steward <me@scala-steward.org>
2021-07-27 14:21:24 +0200

    Update tapir-cats, tapir-http4s-server, ... to 0.19.0-M3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val logback = "1.2.5"
     val sttpClient3 = "3.3.11"
     val sttpModel = "1.4.9"
-    val sttpTapir = "0.19.0-M2"
+    val sttpTapir = "0.19.0-M3"
 
     val kindProjector = "0.13.0"
     val betterMonadicFor = "0.3.1"
