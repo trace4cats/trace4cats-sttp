7e4401dc6756eab0b475ba9af65759b4aa6d41aa
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-07-28 20:38:38 +0300

    Fixes after review

--- modules/sttp-tapir/src/test/scala/io/janstenpickle/trace4cats/sttp/tapir/TracedServerEndpointCtxSpec.scala
+++ modules/sttp-tapir/src/test/scala/io/janstenpickle/trace4cats/sttp/tapir/TracedServerEndpointCtxSpec.scala
@@ -2,13 +2,12 @@ package io.janstenpickle.trace4cats.sttp.tapir
 
 import cats.data.Kleisli
 import cats.effect.IO
-import cats.{~>, Id}
-import io.janstenpickle.trace4cats.sttp.common.TraceContext
+import io.janstenpickle.trace4cats.sttp.common.{RunIOToId, TraceContext}
 import io.janstenpickle.trace4cats.sttp.tapir.Instances._
 
 class TracedServerEndpointCtxSpec
     extends BaseServerEndpointTracerSpec[IO](
-      λ[IO ~> Id](_.unsafeRunSync()),
+      RunIOToId,
       new Endpoints[IO, Kleisli[IO, TraceContext[IO], *]].tracedContextEndpoints(_),
       checkMkContextErrors = true
     )
--- modules/sttp-tapir/src/test/scala/io/janstenpickle/trace4cats/sttp/tapir/TracedServerEndpointSpec.scala
+++ modules/sttp-tapir/src/test/scala/io/janstenpickle/trace4cats/sttp/tapir/TracedServerEndpointSpec.scala
@@ -2,13 +2,12 @@ package io.janstenpickle.trace4cats.sttp.tapir
 
 import cats.data.Kleisli
 import cats.effect.IO
-import cats.{~>, Id}
 import io.janstenpickle.trace4cats.Span
-import io.janstenpickle.trace4cats.sttp.tapir.Instances._
+import io.janstenpickle.trace4cats.sttp.common.RunIOToId
 
 class TracedServerEndpointSpec
     extends BaseServerEndpointTracerSpec[IO](
-      λ[IO ~> Id](_.unsafeRunSync()),
+      RunIOToId,
       new Endpoints[IO, Kleisli[IO, Span[IO], *]].tracedEndpoints(_),
       checkMkContextErrors = false
     )
