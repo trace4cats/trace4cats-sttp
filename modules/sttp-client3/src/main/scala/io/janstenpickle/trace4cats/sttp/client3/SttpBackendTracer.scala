d6222477601a7c47e6d2d917ec98c318f6348ef0
Yannick Heiber <yannick.heiber@mobimeo.com>
2021-09-22 13:58:58 +0200

    Add response attribute extraction (#59)
    * Add response attribute extraction
    
    Supports extracting span attributes depending on the sttp response, for example to have HTTP status codes in traces.
    
    * Add response header extraction
    
    * Apply suggestions from code review
    
    Co-authored-by: catostrophe <40268503+catostrophe@users.noreply.github.com>
    
    * More PR review fixes
    
    * Unify val names for attributes
    
    * Fix variable name in comment
    
    Co-authored-by: catostrophe <40268503+catostrophe@users.noreply.github.com>

--- modules/sttp-client3/src/main/scala/io/janstenpickle/trace4cats/sttp/client3/SttpBackendTracer.scala
+++ modules/sttp-client3/src/main/scala/io/janstenpickle/trace4cats/sttp/client3/SttpBackendTracer.scala
@@ -1,22 +1,26 @@
 package io.janstenpickle.trace4cats.sttp.client3
 
 import cats.effect.kernel.{Async, MonadCancelThrow}
+import cats.syntax.functor._
 import cats.syntax.flatMap._
 import io.janstenpickle.trace4cats.Span
 import io.janstenpickle.trace4cats.base.context.Provide
 import io.janstenpickle.trace4cats.base.optics.{Getter, Lens}
-import io.janstenpickle.trace4cats.model.{SampleDecision, SpanKind, TraceHeaders}
+import io.janstenpickle.trace4cats.model.{AttributeValue, SampleDecision, SpanKind, TraceHeaders}
 import io.janstenpickle.trace4cats.sttp.common.{SttpHeaders, SttpStatusMapping}
 import sttp.capabilities.{Effect => SttpEffect}
 import sttp.client3.impl.cats.implicits._
 import sttp.client3.{HttpError, Request, Response, SttpBackend}
+import sttp.model.Headers
 import sttp.monad.{MonadError => SttpMonadError}
 
 class SttpBackendTracer[F[_], G[_], +P, Ctx](
   backend: SttpBackend[F, P],
   spanLens: Lens[Ctx, Span[F]],
   headersGetter: Getter[Ctx, TraceHeaders],
-  spanNamer: SttpSpanNamer
+  spanNamer: SttpSpanNamer,
+  dropHeadersWhen: String => Boolean,
+  responseAttributesGetter: Getter[Response[_], Map[String, AttributeValue]]
 )(implicit P: Provide[F, G, Ctx], F: MonadCancelThrow[F], G: Async[G])
     extends SttpBackend[G, P] {
   def send[T, R >: P with SttpEffect[G]](request: Request[T, R]): G[Response[T]] =
@@ -35,19 +39,23 @@ class SttpBackendTracer[F[_], G[_], +P, Ctx](
           val lower = P.provideK(childCtx)
           val ctxBackend = backend.mapK(P.liftK, lower)
 
-          val headers = headersGetter.get(childCtx)
-          val req = request.headers(SttpHeaders.converter.to(headers).headers: _*)
+          val ctxHeaders = headersGetter.get(childCtx)
+          val req = request.headers(SttpHeaders.converter.to(ctxHeaders).headers: _*)
 
+          val reqHeaderAttrs = SttpHeaders.requestFields(Headers(req.headers), dropHeadersWhen)
+          val isSampled = childSpan.context.traceFlags.sampled == SampleDecision.Include
           // only extract request attributes if the span is sampled as the host parsing is quite expensive
-          val requestAttributes =
-            if (childSpan.context.traceFlags.sampled == SampleDecision.Include)
-              childSpan.putAll(SttpRequest.toAttributes(request))
-            else F.unit
+          val reqExtraAttrs = if (isSampled) SttpRequest.toAttributes(request) else Map.empty
 
-          requestAttributes >> lower(ctxBackend.send(req))
-            .flatTap { resp =>
-              childSpan.setStatus(SttpStatusMapping.statusToSpanStatus(resp.statusText, resp.code))
-            }
+          for {
+            _ <- childSpan.putAll(reqHeaderAttrs ++ reqExtraAttrs: _*)
+            resp <- lower(ctxBackend.send(req))
+            _ <- childSpan.setStatus(SttpStatusMapping.statusToSpanStatus(resp.statusText, resp.code))
+            respHeaderAttrs = SttpHeaders.responseFields(Headers(resp.headers), dropHeadersWhen)
+            // responseAttributesGetter could be expensive, so only call if the span is sampled
+            respExtraAttrs = if (isSampled) responseAttributesGetter.get(resp) else Map.empty
+            _ <- childSpan.putAll(respHeaderAttrs ++ respExtraAttrs: _*)
+          } yield resp
         }
     }
 
