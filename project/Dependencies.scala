08ba9af11b58f67ea66d1d94203d81c1b4244643
Scala Steward <me@scala-steward.org>
2021-10-01 17:01:06 +0200

    Update model:core to 1.4.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     val http4s = "0.23.4"
     val logback = "1.2.6"
     val sttpClient3 = "3.3.14"
-    val sttpModel = "1.4.11"
+    val sttpModel = "1.4.13"
     val sttpTapir = "0.19.0-M10"
 
     val kindProjector = "0.13.2"
