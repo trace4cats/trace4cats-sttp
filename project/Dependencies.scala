104e094deea12aedecbddbe9237e698a6a983f07
Scala Steward <me@scala-steward.org>
2021-12-23 18:24:52 +0100

    Update logback-classic to 1.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val trace4cats = "0.12.0"
 
     val http4s = "0.23.7"
-    val logback = "1.2.9"
+    val logback = "1.2.10"
     val sttpClient3 = "3.3.18"
     val sttpModel = "1.4.20"
     val sttpTapir = "0.19.3"
