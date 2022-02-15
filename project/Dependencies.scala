bd97f6156b37fe34cbbd016c1874880efec5310b
Scala Steward <me@scala-steward.org>
2022-02-15 19:17:02 +0100

    Update cats, http4s-backend to 3.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
 
     val http4s = "0.23.10"
     val logback = "1.2.10"
-    val sttpClient3 = "3.4.2"
+    val sttpClient3 = "3.5.0"
     val sttpModel = "1.4.23"
     val sttpTapir = "0.19.3"
 
