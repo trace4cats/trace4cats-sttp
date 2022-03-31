a1c1f9769f1e08b184941e322446569c9fd55e82
Scala Steward <me@scala-steward.org>
2022-03-31 05:10:51 +0200

    Update trace4cats-base, trace4cats-core, ... to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val scala213 = "2.13.8"
     val scala3 = "3.1.1"
 
-    val trace4cats = "0.13.0"
+    val trace4cats = "0.13.1"
 
     val http4s = "0.23.11"
     val logback = "1.2.11"
