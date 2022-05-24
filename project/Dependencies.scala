7165eab0b802fc56e80f829e9b7a525fe7b6af39
Scala Steward <me@scala-steward.org>
2022-05-25 01:52:59 +0200

    Update http4s-client, http4s-dsl to 0.23.12

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
     val trace4cats = "0.13.1"
 
-    val http4s = "0.23.11"
+    val http4s = "0.23.12"
     val logback = "1.2.11"
     val sttpClient3 = "3.5.2"
     val sttpModel = "1.4.26"
