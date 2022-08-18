c8a15efc5a1fbd4cdda211bf2551ba67d4715515
Scala Steward <scala_steward@virtuslab.com>
2022-08-18 18:52:09 +0000

    Update tapir-cats, tapir-http4s-server, ... to 1.0.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
     val logback = "1.2.11"
     val sttpClient3 = "3.7.2"
     val sttpModel = "1.5.0"
-    val sttpTapir = "1.0.3"
+    val sttpTapir = "1.0.5"
 
     val kindProjector = "0.13.2"
     val betterMonadicFor = "0.3.1"
